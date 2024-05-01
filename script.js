f2192caccf9f84ed35f7b395c999300e39276de1
Abhishek Farande <farandeabhishek04@gmail.com>
2024-05-02 03:03:11 +0530

    Addition of light and Dark properties

--- script.js
+++ script.js
@@ -51,13 +51,77 @@ input.addEventListener(
                 getUserData(url+input.value);
             }
         }
-    }
-)
+    },
+    false
+);
 
-
+// if value changes for input , then noresults text display will be none
 input.addEventListener("input", function() {
     noresults.style.display = "none";
 });
 
+btnmode.addEventListener("click",function() {
+    if(darkMode==false) {
+        darkModeProperties();
+    } else {
+        lightModeProperties();
+    }
+});
 
 
+// function Call 
+function getUserData(gitUrl) {
+    // alternative to async and await 
+    fetch(gitUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        updateProfile(data);
+     })
+
+    .catch((error) => {
+        throw error;
+     });
+
+}
+
+//Render 
+function updateProfile(data) {
+
+}
+
+
+//SWITCH TO DARK MODE - activateDarkMode()
+function darkModeProperties() {
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-text-alt", "white");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "./assets/images/sun-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(1000%)");
+    darkMode = true;
+    console.log("darkmode changed to " + darkMode);
+    localStorage.setItem("dark-mode", true);  console.log("setting dark mode to false");
+  
+    console.log("setting dark mode to true");
+  
+  }
+  
+  //SWITCH TO LIGHT MODE - activateLightMode()
+  function lightModeProperties() {
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-text-alt", "#2B3442");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "./assets/images/moon-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(100%)");
+    darkMode = false;
+    console.log("darkmode changed to " + darkMode);
+  
+    localStorage.setItem("dark-mode", false);
+    console.log("setting dark mode to false");
+  }
